ab1dec4fe9cc6a0a6d6fef004ca6e2ade0c36512
MarinaGotka <marina.gotka@gmail.com>
2018-03-04 17:24:46 +0300

    First

---
+++ task2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace task_DEV2
+{
+    class Entry
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Intput string: ");
+            string str = Console.ReadLine();
+
+            if (str.Length > 0)
+            {
+                CreatorStringOnlyOfEvenIndexes strEven = new CreatorStringOnlyOfEvenIndexes(str);
+                string res = strEven.BuildSubstringOnlyOfEvenIndexes();
+                Console.WriteLine("String contains with only even indexes: {0} ", res);
+            }
+            else
+            {
+                Console.WriteLine("Error.The string is empty");
+            }
+        }
+    }
+}

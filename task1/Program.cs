a8fb312f33cf4cabccbab0e03aab7615dc45b303
MarinaGotka <marina.gotka@gmail.com>
2018-03-04 16:59:41 +0300

    last solution

---
+++ task1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace task_DEV1
+{
+    class Entry
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 1)
+            {
+                MaxSequence maxSequense = new MaxSequence(args[0]);
+                int maxCount = maxSequense.CalculateMaxNumberOfRepetitions();
+                Console.WriteLine("Maximum = {0}", maxCount);
+            }
+            else
+            {
+                Console.WriteLine("Error.The string is not single or empty");
+            }
+        }
+
+    }
+}
